8bf2cd91a1fb04d6295654e8bafc3f3a5ef0a25d
David Jiang <davidjgrey@gmail>
2016-07-28 12:14:22 -0700

    seeded with reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,34 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-(1..30).each do |i|
-  planet = Tatooine::Planet.get(i)
-  sleep(0.5)
-  name = planet.name
-  population = planet.population
-  climate = planet.climate
-  terrain = planet.terrain
+# (1..30).each do |i|
+#   planet = Tatooine::Planet.get(i)
+#   sleep(0.5)
+#   name = planet.name
+#   population = planet.population
+#   climate = planet.climate
+#   terrain = planet.terrain
 
-  Planet.find_or_create_by(name: name, population: population, climate: climate, terrain: terrain)
-end
+#   Planet.find_or_create_by(name: name, population: population, climate: climate, terrain: terrain)
+# end
 
 PEOPLE = []
 RATINGS = [1,2,3,4,5]
-(1..80).each do |i|
+(1..40).each do |i|
+  next if i == 17
   person = Tatooine::Person.get(i)
   sleep(0.5)
   PEOPLE << person.name
 end
 
 (1..30).each do |i|
-  planet = Planet.find(i)
+  #planet = Planet.find(i)
   10.times do 
     author_name = PEOPLE.sample
     body = "Here is a sample review. The text would go here. You might write something about lodging, food or attractions in the area."
     rating = RATINGS.sample
     planet_id = i
     title = "Sample Review Title"
-    Review.find_or_create_by(author_name: author_name, body: body, rating: rating, planet_id: planet_id)
+    Review.find_or_create_by(author_name: author_name, body: body, rating: rating, planet_id: planet_id, title: title)
   end
 end
